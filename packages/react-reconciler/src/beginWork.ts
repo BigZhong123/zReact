c3cbaf75a8dab95d0ff97676a57d22571970b9a0
bobo <1059675475@qq.com>
2023-10-15 16:07:07 +0800

    feat: 支持FC渲染

--- packages/react-reconciler/src/beginWork.ts
+++ packages/react-reconciler/src/beginWork.ts
@@ -1,8 +1,14 @@
 import { ReactElementType } from "shared/ReactTypes";
 import { FiberNode } from "./fiber";
 import { UpdateQueue, processUpdateQueue } from "./updateQueue";
-import { HostComponent, HostRoot, HostText } from "./workTags";
+import {
+  FunctionComponent,
+  HostComponent,
+  HostRoot,
+  HostText,
+} from "./workTags";
 import { mountChildFibers, reconcilerChildFibers } from "./childFibers";
+import { renderWithHooks } from "./fiberHooks";
 
 // 递归中的递
 export const beginWork = (wip: FiberNode) => {
@@ -14,6 +20,8 @@ export const beginWork = (wip: FiberNode) => {
       return updateHostComponent(wip);
     case HostText:
       return null;
+    case FunctionComponent:
+      return updateFunctionComponent(wip);
     default:
       if (__DEV__) {
         console.warn("beginWork未实现的类型");
@@ -22,6 +30,14 @@ export const beginWork = (wip: FiberNode) => {
   }
   return null;
 };
+
+function updateFunctionComponent(wip: FiberNode) {
+  const nextChildren = renderWithHooks(wip);
+
+  reconcilerChildren(wip, nextChildren);
+
+  return wip.child;
+}
 
 function updateHostRoot(wip: FiberNode) {
   const baseState = wip.memoizedState;
