c90143cfecf6e9b5a13e8d74320f3f40d1afbbae
bobo <1059675475@qq.com>
2023-12-07 00:07:53 +0800

    feat: 实现useEffect数据结构

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -4,5 +4,8 @@ export const NoFlags = 0b000000;
 export const Placement = 0b000001;
 export const Update = 0b000010;
 export const ChildDeletion = 0b000100;
+export const PassiveEffect = 0b001000;
 
 export const MutationMask = Placement | Update | ChildDeletion;
+
+export const PassiveMask = PassiveEffect | ChildDeletion;
---
+++ packages/react-reconciler/src/hookEffectTags.ts
@@ -0,0 +1,2 @@
+export const Passiive = 0b0010;
+export const HookHasEffect = 0b0001;
